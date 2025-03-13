53398d2549d74e85f4ce2bfa27959278055f8dce
EllaCallas <ella.callas@icloud.com>
2025-03-13 16:25:40 +0100

    Add ivabradine to drugs, update processing codes to extract relevant variables

--- Ella/CodeFromSB/UsefulProcessingCodesForElla_Matlab.m
+++ Ella/CodeFromSB/UsefulProcessingCodesForElla_Matlab.m
@@ -1,6 +1,10 @@
 %% Get noisy epochs
+clear all
+load('ChannelsToAnalyse/Bulb_deep.mat');
+LowSpectrumSB([cd filesep],channel,'B');
+% FindNoiseEpoch_BM([cd filesep],channel,0);
 FindNoiseEpoch_BM([cd filesep],channel,0);
-              
+             
 %% Get heart beats
 clear all
 Options.TemplateThreshStd=3;
@@ -8,28 +12,30 @@ Options.BeatThreshStd=0.05;
 load('ChannelsToAnalyse/EKG.mat')
 load(['LFPData/LFP',num2str(channel),'.mat'])
 load('StateEpochSB.mat','TotalNoiseEpoch')
-[Times,Template,HeartRate,GoodEpoch] = DetectHeartBeats_EmbReact_SB(LFP,NoiseEpoch,Options,1);
+% load('StateEpochSB.mat','NoiseEpoch')
+[Times,Template,HeartRate,GoodEpoch] = DetectHeartBeats_EmbReact_SB(LFP,TotalNoiseEpoch,Options,1);
 
-EKG.HBTimes=ts(Times);l
+EKG.HBTimes=ts(Times);
 EKG.HBShape=Template;
 EKG.DetectionOptions=Options;
 EKG.HBRate=HeartRate;
 EKG.GoodEpoch=GoodEpoch;
 
 save('HeartBeatInfo.mat','EKG')
-saveas(1,'EKGCheck.fig'),
+saveas(1,'EKGCheck.fig')
 saveas(1,'EKGCheck.png')
 
 %% Get gamma power
 clear all
-load('ChannelsToAnalyse/EKG.mat')
+load('ChannelsToAnalyse/Bulb_deep.mat');
 load(['LFPData/LFP',num2str(channel),'.mat'])
 FilGamma=FilterLFP(LFP,[50 70],1024);
 HilGamma=hilbert(Data(FilGamma));
 H=abs(HilGamma);
 tot_ghi=tsd(Range(LFP),H);
+smootime = 3;
 SmoothGamma = tsd(Range(tot_ghi),runmean(Data(tot_ghi),ceil(smootime/median(diff(Range(tot_ghi,'s'))))));
-save('SleepScoring_OBGamma','Sleep','SmoothGamma','-append')
+save('SleepScoring_OBGamma','SmoothGamma','-append')
 
 %% Sleep scoring
 SleepScoringOBGamma
