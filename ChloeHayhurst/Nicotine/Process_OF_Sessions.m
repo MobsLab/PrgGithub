669118b78efbe0fb0f41a23fedf640ca89881235
Chloe Hayhurst <hayhurst2112@gmail.com>
2025-05-05 16:58:08 +0200

    Code to process OF sessions : AlignedXtsd, all Spectros and HR

---
+++ ChloeHayhurst/Nicotine/Process_OF_Sessions.m
@@ -0,0 +1,81 @@
+clear all, close all
+load('behavResources.mat')
+
+
+if not(exist('B_Low_Spectrum.mat'))
+    load('ChannelsToAnalyse/Bulb_deep.mat')
+    channel;
+    LowSpectrumSB([cd filesep],channel,'B')
+end
+if not(exist('StateEpochSB.mat'))
+    load('ChannelsToAnalyse/Bulb_deep.mat')
+    channel;
+    FindNoiseEpoch_BM([cd filesep],channel,0);
+end
+
+CreateRipplesSleep('stim',0,'restrict',0,'sleep',0,'plotavg',0)
+
+
+thtps_immob=2;
+smoofact_Acc = 30;
+th_immob_Acc = 1.7e7;
+
+NewMovAcctsd=tsd(Range(MovAcctsd),runmean(Data(MovAcctsd),smoofact_Acc));
+FreezeAccEpoch=thresholdIntervals(NewMovAcctsd,th_immob_Acc,'Direction','Below');
+FreezeAccEpoch=mergeCloseIntervals(FreezeAccEpoch,0.3*1e4);
+FreezeAccEpoch=dropShortIntervals(FreezeAccEpoch,thtps_immob*1e4);
+
+save('behavResources.mat', 'FreezeAccEpoch','-append');
+
+clear channel
+if not(exist('HeartBeatInfo.mat'))
+    close all
+    try
+        clear EKG channel
+        Options.TemplateThreshStd=3;
+        Options.BeatThreshStd=0.05;
+        load('ChannelsToAnalyse/EKG.mat')
+        load(['LFPData/LFP',num2str(channel),'.mat'])
+        load('StateEpochSB.mat','TotalNoiseEpoch')
+        load('ExpeInfo.mat')
+        load('behavResources.mat')
+        NoiseEpoch=TotalNoiseEpoch;
+        [Times,Template,HeartRate,GoodEpoch]=DetectHeartBeats_EmbReact_SB(LFP,NoiseEpoch,Options,1);
+        EKG.HBTimes=ts(Times);
+        EKG.HBShape=Template;
+        EKG.DetectionOptions=Options;
+        EKG.HBRate=HeartRate;
+        EKG.GoodEpoch=GoodEpoch;
+        
+        save('HeartBeatInfo.mat','EKG')
+        saveas(1,'EKGCheck.fig'),
+        saveas(1,'EKGCheck.png')
+        close all
+        clear EKG NoiseEpoch TotalNoiseEpoch TTLInfo LFP EKG HearRate Template Times
+    end
+end
+
+satisfied = 0;
+if not(exist('AlignedXtsd','var'))
+    while satisfied ==0
+        
+        X=Data(Xtsd);Y=Data(Ytsd);
+        figure
+        %         imagesc(double(ref)), colormap jet, hold on
+        plot(X,Y)
+        title('give 3 corners : bottom left, bottom right and top left')
+        [x,y] = ginput(3);
+        
+        close all
+        
+        AlignedXtsd = tsd(Range(Xtsd),(X-x(1))./(x(2)-x(1)));
+        AlignedYtsd = tsd(Range(Ytsd),(Y-y(1))./(y(3)-y(1)));
+        
+        figure, hold on
+        plot(Data(AlignedXtsd),Data(AlignedYtsd))
+        
+        satisfied = input('Satisfied?')
+    end
+end
+save('behavResources.mat', 'AlignedXtsd', 'AlignedYtsd','-append');
+
